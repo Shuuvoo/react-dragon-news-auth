56936261cfd31ef88dfe4d6654db1dd230f85db3
Shuuvoo <shekhoshuvo@gmail.com>
2023-10-04 20:04:16 +0600

    Marquee Breakin news and Navbar added completed

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -3,8 +3,8 @@ import logo from '../../../assets/logo.png'
 
 const Header = () => {
     return (
-        <div className=''>
-            <img src={logo} alt="" />
+        <div className='text-center'>
+            <img className='mx-auto my-3' src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
             <p className='text-xl'>{moment().format("dddd, MMMM D YYYY, h:mm:ss a")}</p>
         </div>
