52f7fbb0f3b5a661d0cc7b1cdb8dee610ae77a33
Shuuvoo <shekhoshuvo@gmail.com>
2023-10-04 18:29:57 +0600

    Lefeside nav and right side nav basic component created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            <h1 className="text-5xl text-center font-bold">This is Home Page </h1>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h1 className="text-5xl  font-poppins font-bold">This is Home Page </h1>
         </div>
     );
 };
