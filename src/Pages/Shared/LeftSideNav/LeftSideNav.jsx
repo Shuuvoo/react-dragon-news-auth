4aafe026e70857e5e35ef2a308f31a8c41394021
Shuuvoo <shekhoshuvo@gmail.com>
2023-10-04 21:04:25 +0600

    Left Side nav All Category fetch and show them successfully

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,11 +1,29 @@
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-    return (
-        <div>
-            <h2 className="text-2xl">leftSide </h2>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div className="space-y-5">
+      <h2 className="text-2xl text-center font-extrabold">All Category</h2>
+      {categories.map((category) => (
+        <Link
+          className="block ml-4 text-xl font-medium px-4"
+          key={category.id}
+          to={`/category/${category.id}`}
+          category={category}
+        >
+          {category.name}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default LeftSideNav;
